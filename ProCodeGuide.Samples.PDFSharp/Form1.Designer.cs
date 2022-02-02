4301ffd97cef8ea109652e9d417be400c4f0f7b0
Sanjay <support@procodeguide.com>
2022-02-02 09:42:11 +0530

    Add project files.

---
+++ ProCodeGuide.Samples.PDFSharp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ProCodeGuide.Samples.PDFSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGeneratePDF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGeneratePDF
+            // 
+            this.btnGeneratePDF.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGeneratePDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGeneratePDF.Location = new System.Drawing.Point(192, 131);
+            this.btnGeneratePDF.Name = "btnGeneratePDF";
+            this.btnGeneratePDF.Size = new System.Drawing.Size(156, 53);
+            this.btnGeneratePDF.TabIndex = 0;
+            this.btnGeneratePDF.Text = "Generate PDF";
+            this.btnGeneratePDF.UseVisualStyleBackColor = false;
+            this.btnGeneratePDF.Click += new System.EventHandler(this.btnGeneratePDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 304);
+            this.Controls.Add(this.btnGeneratePDF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGeneratePDF;
+    }
+}
+
---
+++ ProCodeGuide.Samples.PDFSharp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProCodeGuide.Samples.PDFSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeneratePDF_Click(object sender, EventArgs e)
+        {
+            //Create PDF Document
+            PdfDocument document = new PdfDocument();
+            
+            //You will have to add Page in PDF Document
+            PdfPage page = document.AddPage();
+            
+            //For drawing in PDF Page you will nedd XGraphics Object
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            
+            //For Test you will have to define font to be used
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            //Finally use XGraphics & font object to draw text in PDF Page
+            gfx.DrawString("My First PDF Document", font, XBrushes.Black,
+                new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+            //Specify file name of the PDF file
+            string filename = "FirstPDFDocument.pdf";
+
+            //Save PDF File
+            document.Save(filename);
+
+            //Load PDF File for viewing
+            Process.Start(filename);
+        }
+    }
+}
